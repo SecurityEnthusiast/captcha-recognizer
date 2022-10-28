a260af5f7c7f5b0bba92dc7772b7f024d7568311
Meghdad Shamsaei <meghdad@shamsaei.com>
2022-10-28 20:49:48 +0200

    Updated config file

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-# about captcha image
+# Captcha image properties
 IMAGE_HEIGHT = 48
 IMAGE_WIDTH = 128
-CHAR_SETS = 'abcdefghijklmnpqrstuvwxyz123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'
-CLASSES_NUM = len(CHAR_SETS)
+CHAR_SET = 'abcdefghijklmnpqrstuvwxyz123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'
+CLASSES_NUM = len(CHAR_SET)
 CHARS_NUM = 5
-# for train
+# Train data folder and records addresses
 RECORD_DIR = './data'
 TRAIN_FILE = 'train.tfrecords'
 VALID_FILE = 'valid.tfrecords'
