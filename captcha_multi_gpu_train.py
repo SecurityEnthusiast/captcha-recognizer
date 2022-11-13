2081309df5eb826cada8fbbdaf17198fb2388613
Meghdad Shamsaei <meghdad@shamsaei.com>
2022-11-13 02:06:27 +0100

    Compatible with Python 3.10, TF 2.10.0, NP 1.23.4 - Not tested

--- captcha_multi_gpu_train.py
+++ captcha_multi_gpu_train.py
@@ -8,7 +8,6 @@ import argparse
 import sys
 
 import numpy as np
-from six.moves import xrange 
 import tensorflow as tf
 import captcha_model as captcha
 
@@ -18,7 +17,7 @@ def tower_loss(scope, keep_prob):
   images, labels = captcha.inputs(train=True, batch_size=FLAGS.batch_size)
   logits = captcha.inference(images, keep_prob)
   _ = captcha.loss(logits, labels)
-  losses = tf.get_collection('losses', scope)
+  losses = tf.compat.v1.get_collection('losses', scope)
   total_loss = tf.add_n(losses, name='total_loss')
   return total_loss
 
@@ -40,28 +39,28 @@ def average_gradients(tower_grads):
 
 def run_train():
   with tf.Graph().as_default(), tf.device('/cpu:0'):
-    opt = tf.train.AdamOptimizer(1e-4)
+    opt = tf.compat.v1.train.AdamOptimizer(1e-4)
     tower_grads = []
-    with tf.variable_scope(tf.get_variable_scope()):
-      for i in xrange(FLAGS.num_gpus):
+    with tf.compat.v1.variable_scope(tf.compat.v1.get_variable_scope()):
+      for i in range(FLAGS.num_gpus):
         with tf.device('/gpu:%d' % i):
           with tf.name_scope('tower_%d' % (i)) as scope:
             loss = tower_loss(scope, keep_prob=0.5)
-            tf.get_variable_scope().reuse_variables()
+            tf.compat.v1.get_variable_scope().reuse_variables()
             grads = opt.compute_gradients(loss)
             tower_grads.append(grads)
 
     grads = average_gradients(tower_grads)
     train_op = opt.apply_gradients(grads)
-    saver = tf.train.Saver(tf.global_variables())
-    init = tf.global_variables_initializer()
-    sess = tf.Session(config=tf.ConfigProto(
+    saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables())
+    init = tf.compat.v1.global_variables_initializer()
+    sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(
         allow_soft_placement=True,
         log_device_placement=True))
     
     sess.run(init)
-    tf.train.start_queue_runners(sess=sess)
-    for step in xrange(FLAGS.max_steps):
+    tf.compat.v1.train.start_queue_runners(sess=sess)
+    for step in range(FLAGS.max_steps):
       start_time = time.time()
       _, loss_value = sess.run([train_op, loss])
       duration = time.time() - start_time
@@ -84,9 +83,9 @@ def run_train():
    
         
 def main(_):
-  if tf.gfile.Exists(FLAGS.train_dir):
-    tf.gfile.DeleteRecursively(FLAGS.train_dir)
-  tf.gfile.MakeDirs(FLAGS.train_dir)
+  if tf.io.gfile.exists(FLAGS.train_dir):
+    tf.io.gfile.rmtree(FLAGS.train_dir)
+  tf.io.gfile.makedirs(FLAGS.train_dir)
   run_train()
 
 
@@ -107,7 +106,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '--num_gpus',
       type=int,
-      default=8,
+      default=1,
       help='How many GPUs to use.'
   )
   parser.add_argument(
@@ -123,4 +122,4 @@ if __name__ == '__main__':
       help='Directory where to write checkpoint.'
   )
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  tf.compat.v1.app.run(main=main, argv=[sys.argv[0]] + unparsed)
